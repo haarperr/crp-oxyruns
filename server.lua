500f75ce06e47944341d0b0d9c097462f81f36dd
dappa <sexplosive42@gmail.com>
2021-04-21 13:43:11 +0700

    rl to qb

--- server.lua
+++ server.lua
@@ -1,22 +1,22 @@
-RLCore = nil
-TriggerEvent("RLCore:GetObject",function(obj) RLCore = obj end)
+QBCore = nil
+TriggerEvent("QBCore:GetObject",function(obj) QBCore = obj end)
 
 RegisterServerEvent('oxy:serverPay')
 AddEventHandler('oxy:serverPay', function(money)
     local src = source
-    local User = RLCore.Functions.GetPlayer(src)
+    local User = QBCore.Functions.GetPlayer(src)
     if User.PlayerData["money"]["cash"] >= 1500 then
         User.Functions.RemoveMoney('cash', money)
         TriggerClientEvent("oxyrun:startOxyRun", src)
     else
-        TriggerClientEvent("RLCore:Notify", src, "You don't have enough Money for this!", "error") 
+        TriggerClientEvent("QBCore:Notify", src, "You don't have enough Money for this!", "error") 
     end
 end)
 
 RegisterServerEvent('oxy:moneyforPackage')
 AddEventHandler('oxy:moneyforPackage', function()
     local src = source
-    local User = RLCore.Functions.GetPlayer(src)
+    local User = QBCore.Functions.GetPlayer(src)
     local amount = math.random(Config.MinAmount, Config.MaxAmount)
     User.Functions.AddMoney("cash", amount)
 end)
