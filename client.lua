dfb5739bd176b09bd42e8b98ad61dcd96f72b1b1
dappa <sexplosive42@gmail.com>
2021-04-21 15:48:20 +0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,5 +1,11 @@
 QBCore = nil
-TriggerEvent("QBCore:GetObject",function(obj) QBCore = obj end)
+
+Citizen.CreateThread(function()
+	while QBCore == nil do
+		TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+		Citizen.Wait(200)
+	end
+end)
 
 started = false
 gotdest = false
